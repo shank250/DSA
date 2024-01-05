49ac195ddc007c4d0f67466602b63468907a25c0
Shashank Srivastava <114741573+shank250@users.noreply.github.com>
2024-01-05 22:28:17 +0530

    solved ldq

---
+++ Python DSA/Leetcode_daily/05012024.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # max_ss = 0
+        # counter = 0
+        # for i in nums:
+        #     if i == nums[0]:
+        #         prev = i
+        #     else:
+        #         if prev < i:
+        #             counter += 1
+        #             if counter > max_ss:
+        #                 max_ss = counter
+        #         else:
+        #             counter = 0
+        # return max_ss
+        # def lengthOfLIS(nums):
+        if not nums:
+            return 0
+
+        n = len(nums)
+        dp = [1] * n
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return max(dp)
